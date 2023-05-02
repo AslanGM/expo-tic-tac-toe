173640021a82be822ab5a3e3971ef9cedfe19f61
AslanGoat <aslan@goat.digital>
2023-05-02 15:27:56 +0600

    update home screen

--- src/screens/home/home.styles.ts
+++ src/screens/home/home.styles.ts
@@ -2,7 +2,19 @@ import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingTop: 120
+    },
+    logo: {
+        width: '60%',
+        height: 120,
+        resizeMode: 'contain'
+    },
+    buttonsContainer: {
+        marginTop: 40,
+        padding: 20,
+        width: '100%',
+        gap: 12
     }
 })
 
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,8 +1,8 @@
-import { Background, Text } from '@components'
+import { Background, Button } from '@components'
 import { StackNavigatorParams } from '@config/navigator'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { ReactElement } from 'react'
-import { Button, ScrollView } from 'react-native'
+import { Image, ScrollView, View } from 'react-native'
 import styles from './home.styles'
 
 type HomeProps = {
@@ -13,13 +13,16 @@ export default function Home({ navigation }: HomeProps): ReactElement {
     return (
         <Background>
             <ScrollView contentContainerStyle={styles.container}>
-                <Text>Home</Text>
-                <Button
-                    title="Go to Game"
-                    onPress={(): void => {
-                        navigation.navigate('Game')
-                    }}
+                <Image
+                    source={require('@assets/images/logo.png')}
+                    style={styles.logo}
                 />
+                <View style={styles.buttonsContainer}>
+                    <Button title="Single Player" />
+                    <Button title="Multiplayer" />
+                    <Button title="Login" />
+                    <Button title="Settings" />
+                </View>
             </ScrollView>
         </Background>
     )
