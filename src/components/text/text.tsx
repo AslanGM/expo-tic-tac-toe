d0a142b580eb0050e3ce1d8e0f317c7e10085422
AslanGoat <aslan@goat.digital>
2023-05-02 15:27:46 +0600

    update text component

--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -3,14 +3,12 @@ import { Text as NativeText, TextProps as NativeTextProps } from 'react-native'
 
 type TextProps = {
     weight: '400' | '700'
-    color: string
     textTransform: 'uppercase' | 'lowercase' | 'capitalize'
     children: ReactNode
 } & NativeTextProps
 
 const defaultProps = {
     weight: '700',
-    color: '#FAFF18',
     textTransform: 'uppercase'
 }
 
@@ -18,7 +16,6 @@ export default function Text({
     children,
     style,
     weight,
-    color,
     textTransform,
     ...props
 }: TextProps): ReactElement {
@@ -32,7 +29,7 @@ export default function Text({
     return (
         <NativeText
             {...props}
-            style={[{ fontFamily, color, textTransform }, style]}
+            style={[{ fontFamily, textTransform }, style]}
         >
             {children}
         </NativeText>
